c81ad317a8793eba6779bdba4241074f6c9f52eb
Baptiste <130666654+baptistecjn@users.noreply.github.com>
2025-04-10 20:41:35 +0200

    Delete Maths/Maths directory

--- Maths/Maths/TP1_PROJ_MATHS_COJEAN_AUFFRET/exercice4/estConvexe.m
+++
@@ -1,8 +0,0 @@
-function result = estConvexe(A, B, C, D)
-    cross1 = cross([B - A, 0], [C - B, 0]);
-    cross2 = cross([C - B, 0], [D - C, 0]);
-    cross3 = cross([D - C, 0], [A - D, 0]);
-    cross4 = cross([A - D, 0], [B - A, 0]);
-    
-    result = (sign(cross1(3)) == sign(cross2(3))) && (sign(cross2(3)) == sign(cross3(3))) && (sign(cross3(3)) == sign(cross4(3)));
-end
--- Maths/Maths/TP1_PROJ_MATHS_COJEAN_AUFFRET/exercice4/intersectionLigneTriangle.m
+++
@@ -1,8 +0,0 @@
-function result = intersectionLigneTriangle(A, B, C, normal, P)
-    d = dot(normal, P - A);
-    if abs(d) < 1e-10
-        result = pointDansTriangle(P, A, B, C);
-    else
-        result = false;
-    end
-end
